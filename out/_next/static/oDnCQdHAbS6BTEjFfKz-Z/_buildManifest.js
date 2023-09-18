91474e1500caa9c9769c8ce29540f2b4d9e6b4b9
zuno90 <pnlan1406@gmail.com>
2023-09-18 20:06:33 +0700

    full

---
+++ out/_next/static/oDnCQdHAbS6BTEjFfKz-Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/571-84ba8171237a3a07.js","static/css/ae4ed9c503fd1e33.css","static/chunks/pages/index-680a2be3cd747d11.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
